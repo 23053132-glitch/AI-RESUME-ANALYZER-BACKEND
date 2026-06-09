c7e21950ad64d1e70552386442b3239420dad1ff
khushboo <23053132@kiit.ac.in>
2026-06-09 09:41:59 +0530

    fix: add vercel to cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const express = require("express")
-//app.js is used to create instances and apiroutes(endcall)
 const cookiesParser = require("cookie-parser");
 const cors = require("cors")
 const app = express();
@@ -8,16 +7,18 @@ app.use(cookiesParser());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "https://ai-resume-analyzer-n9kkjozsm-23053132-glitchs-projects.vercel.app"
+    ],
     credentials: true
 }))
-/*Requires all the routes here */
+
 const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
-/*using all the routes*/
-app.use("/api/auth",authRouter);
-app.use("/api/interview",interviewRouter);
 
-
+app.use("/api/auth", authRouter);
+app.use("/api/interview", interviewRouter);
 
 module.exports = app;
